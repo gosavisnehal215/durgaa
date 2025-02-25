d618ff2c7544e63324e2070646145387849fdad8
Krushna <girikrushna8888@gmail.com>
2025-02-25 08:27:43 -0800

    temprarory commit

--- src/Abstarct_class/Advantage.java
+++ src/Abstarct_class/Advantage.java
@@ -11,7 +11,7 @@ package Abstarct_class;
     abstract void start(); //abstarct method
 
     void stop(){ // normal method
-        System.out.println(brand +"is stopping.");
+        System.out.println(brand +"is sssstopping.");
     }
 }
 //first child class
