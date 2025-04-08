ff20ac6bb03d7d227b27387ebbc8b90440551281
Kiran <kirangiri789@gmail.com>
2025-04-08 18:28:43 +0530

    loop program

--- src/Loop/For_loop.java
+++ src/Loop/For_loop.java
@@ -3,6 +3,6 @@ package Loop;
 public class For_loop {
     public static void main(String[] args) {
 
-        System.out.println("helsdfsdflsadaso");
+        System.out.println("helsdfsdfdsddlsadaso");
     }
 }
